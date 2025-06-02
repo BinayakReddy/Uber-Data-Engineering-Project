463abe626e4b4de7d9bfdb5bb595762fdb48260b
Binayak Reddy <77384106+BinayakReddy@users.noreply.github.com>
2025-06-02 12:59:01 +0530

    Update and rename load.py to Data Exporter.py

--- mage-files/Data Exporter.py
+++ mage-files/Data Exporter.py
@@ -1,4 +1,4 @@
-from mage_ai.data_preparation.repo_manager import get_repo_path
+from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.bigquery import BigQuery
 from mage_ai.io.config import ConfigFileLoader
 from pandas import DataFrame
@@ -9,22 +9,20 @@ if 'data_exporter' not in globals():
 
 
 @data_exporter
-def export_data_to_big_query(data, **kwargs) -> None:
+def export_data_to_big_query(df, **kwargs) -> None:
     """
     Template for exporting data to a BigQuery warehouse.
     Specify your configuration settings in 'io_config.yaml'.
 
     Docs: https://docs.mage.ai/design/data-loading#bigquery
-
-    
     """
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
 
-    for key, value in data.items():
-        table_id = 'uber-analytics-project-461102.Uber_data_engineering.{}'.format(key)
+    for key,value in df.items():
+        table_id = 'uber-analytics-project-461102.uber_data_eng.{}'.format(key)
         BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
-            DataFrame(value),
-            table_id,
-            if_exists='replace',  # Specify resolution policy if table name already exists
-        )
+        DataFrame(value),
+        table_id,
+        if_exists='replace',  # Specify resolution policy if table name already exists
+    )
